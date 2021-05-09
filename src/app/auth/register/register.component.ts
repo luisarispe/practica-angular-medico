61128954468f7c2aab00ef0d7d1be70a735d8ac5
Luis Arispe Vilca <luis.arispe1991@gmail.com>
2021-05-09 17:49:02 -0400

    formulario reactivo

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
+  public formSumitted = false;
 
-  ngOnInit(): void {
+  public registerForm = this.fb.group({
+    nombre: ['', [Validators.required, Validators.minLength(3)]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+    password2: ['', Validators.required],
+    terminos: [false, Validators.required]
+  }, {
+    validators: this.passwordsIguales()
+  } as AbstractControlOptions);
+
+  constructor(private fb: FormBuilder) { }
+
+  crearUsuario() {
+    this.formSumitted = true;
+    console.log(this.registerForm);
+    if (this.registerForm.valid) {
+      console.log('posteando algo');
+    } else {
+      console.log('Formulario no posteado');
+    }
+  }
+  get getControl() {
+    return this.registerForm.controls;
+  }
+
+  aceptaTerminos(campo: string): boolean {
+    if (!this.registerForm.get(campo)?.value && this.formSumitted) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  passwordsIguales() {
+
+    return (formGroup: FormGroup) => {
+
+      const pass1Control = formGroup.controls['password'];
+      const pass2Control = formGroup.controls['password2'];
+
+      pass1Control?.value === pass2Control?.value ? pass2Control?.setErrors(null) : pass2Control?.setErrors({ noEsIgual: true })
+
+    }
   }
 
 }
+
