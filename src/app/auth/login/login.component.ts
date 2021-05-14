306574b04d4c1c8f1f7f7127da178c04c659124e
Luis Arispe Vilca <luis.arispe1991@gmail.com>
2021-05-13 21:06:31 -0400

    integrando login google

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -4,6 +4,8 @@ import { FormBuilder, Validators } from '@angular/forms';
 import { UsuarioService } from '../../services/usuario.service';
 import Swal from 'sweetalert2';
 
+declare const gapi: any;
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -12,7 +14,7 @@ import Swal from 'sweetalert2';
 export class LoginComponent implements OnInit {
 
   public loginForm = this.fb.group({
-    email: [ localStorage.getItem('email') || '', [Validators.required, Validators.email]],
+    email: [localStorage.getItem('email') || '', [Validators.required, Validators.email]],
     password: ['', Validators.required],
     recordar: [false]
   });
@@ -24,6 +26,7 @@ export class LoginComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.renderButton();
   }
   login() {
     if (this.loginForm.invalid) {
@@ -31,9 +34,9 @@ export class LoginComponent implements OnInit {
     }
 
     this.usuarioService.login(this.loginForm.value).subscribe(resp => {
-      if(this.loginForm.get('recordar')?.value){
+      if (this.loginForm.get('recordar')?.value) {
         localStorage.setItem('email', this.loginForm.get('email')?.value);
-      }else{
+      } else {
         localStorage.removeItem('email');
       }
     }, error => {
@@ -41,5 +44,27 @@ export class LoginComponent implements OnInit {
     });
     // console.log(this.loginForm.value);
   }
+
+  onSuccess(googleUser: any) {
+    // console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log(id_token);
+  }
+  onFailure(error: any) {
+    console.log(error);
+  }
+
+  renderButton() {
+    gapi.signin2.render('my-signin2', {
+      'scope': 'profile email',
+      'width': 240,
+      'height': 50,
+      'longtitle': true,
+      'theme': 'dark',
+      'onsuccess': this.onSuccess,
+      'onfailure': this.onFailure
+    });
+  }
+
 
 }
