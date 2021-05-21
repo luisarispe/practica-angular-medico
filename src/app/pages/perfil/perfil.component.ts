e79d5df3ce25fb1022661dee52e2608aa3fc6d4c
Luis Arispe Vilca <luis.arispe1991@gmail.com>
2021-05-21 12:28:53 -0400

    finalización de actualización de usuario

--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -3,6 +3,8 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Usuario } from 'src/app/models/usuario.model';
 import { UsuarioService } from '../../services/usuario.service';
 import { FileUploadService } from '../../services/file-upload.service';
+import Swal from 'sweetalert2';
+import { switchAll } from 'rxjs/operators';
 
 @Component({
   selector: 'app-perfil',
@@ -13,6 +15,7 @@ import { FileUploadService } from '../../services/file-upload.service';
 export class PerfilComponent implements OnInit {
   public usuario?: Usuario;
   public imageSubir?: File;
+  public imgTemp: any = null;
   public perfilForm = this.fb.group({
     nombre: ['', Validators.required],
     email: ['', [Validators.required, Validators.email]]
@@ -21,7 +24,7 @@ export class PerfilComponent implements OnInit {
 
 
   constructor(private fb: FormBuilder, private usuarioService: UsuarioService,
-             private fileUploadService: FileUploadService) {
+    private fileUploadService: FileUploadService) {
     this.usuario = usuarioService.usuario;
     this.perfilForm.setValue({ 'nombre': this.usuario?.nombre, 'email': this.usuario?.email })
   }
@@ -35,14 +38,37 @@ export class PerfilComponent implements OnInit {
       const { nombre, email } = this.perfilForm.value;
       this.usuario!.nombre = nombre;
       this.usuario!.email = email;
+
+      Swal.fire('Guardado', 'Cambios fueron guardados.', 'success');
+    }, (error) => {
+      Swal.fire('Error', error.error.msg, 'error');
     })
   }
-  cambiarImagen(event:any){
-    this.imageSubir=event.currentTarget.files[0];
+  cambiarImagen(event: any) {
+    this.imageSubir = event.currentTarget.files[0];
+
+    if (!event.currentTarget.files[0]) {
+      this.imgTemp = null;
+      return
+    }
+    const reader = new FileReader();
+    reader.readAsDataURL(event.currentTarget.files[0]);
+    reader.onload = () => {
+      this.imgTemp = reader.result;
+    }
+
   }
-  subirImagen(){
-    this.fileUploadService.actualizarFoto(this.imageSubir!, 'usuario', this.usuario?.uid!).then(img=>{
-      console.log(img);
+  subirImagen() {
+    this.fileUploadService.actualizarFoto(this.imageSubir!, 'usuarios', this.usuario?.uid!).then(img => {
+      if (!img) {
+        Swal.fire('Error', 'No se pudo subir la imagen', 'error');
+      } else {
+        this.usuario!.img = img;
+        Swal.fire('Guardado', 'Imagen de usuario actualizada.', 'success');
+      }
+
+    }).catch(err => {
+      Swal.fire('Error', 'No se pudo subir la imagen', 'error');
     })
   }
 
--- src/app/services/file-upload.service.ts
+++ src/app/services/file-upload.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
-const base_url=environment.base_url;
+const base_url = environment.base_url;
 
 @Injectable({
   providedIn: 'root'
@@ -11,27 +11,31 @@ export class FileUploadService {
   constructor() { }
 
   async actualizarFoto(
-    archivo:File,
-    tipo:'usuario'| 'medicos' | 'hospitales',
-    id:string
-  ){
+    archivo: File,
+    tipo: 'usuarios' | 'medicos' | 'hospitales',
+    id: string
+  ) {
     try {
-      const url= `${base_url}/upload/${tipo}/{id}`;
-      const forData=new FormData();
+      const url = `${base_url}/upload/${tipo}/${id}`;
+      const forData = new FormData();
       forData.append('imagen', archivo);
 
-      const resp= await fetch(url, {
-        method:'PUT',
-        headers:{
+      const resp = await fetch(url, {
+        method: 'PUT',
+        headers: {
           'x-token': localStorage.getItem('token') || ''
         },
-        body: forData  
+        body: forData
       })
-      console.log(resp);
-      return true;
+      const data = await resp.json();
+
+      if (data.ok) {
+        return data.nombreArchivo;
+      } else {
+        return false;
+      }
 
     } catch (error) {
-      console.log(error);
       return false;
     }
   }
