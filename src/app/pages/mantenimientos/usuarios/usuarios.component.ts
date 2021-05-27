47831ae3be722fd0b12a012f43af49d1c643c06a
Luis Arispe Vilca <luis.arispe1991@gmail.com>
2021-05-27 18:16:46 -0400

    opción de busqueda en mantenimiento usuario

--- src/app/pages/mantenimientos/usuarios/usuarios.component.ts
+++ src/app/pages/mantenimientos/usuarios/usuarios.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Usuario } from 'src/app/models/usuario.model';
 import { UsuarioService } from '../../../services/usuario.service';
+import { BusquedasService } from '../../../services/busquedas.service';
 
 @Component({
   selector: 'app-usuarios',
@@ -9,34 +10,45 @@ import { UsuarioService } from '../../../services/usuario.service';
   ]
 })
 export class UsuariosComponent implements OnInit {
-  public totalUsuarios:number=0;
-  public usuarios:Usuario[]=[];
-  public desde:number=0;
-  public cargando: boolean=true;
+  public totalUsuarios: number = 0;
+  public usuarios: Usuario[] = [];
+  public usuariosTemp: Usuario[] = [];
+  public desde: number = 0;
+  public cargando: boolean = true;
 
-  constructor(private usuarioService: UsuarioService) { }
+  constructor(private usuarioService: UsuarioService, private busquedaService: BusquedasService) { }
 
   ngOnInit(): void {
     this.cargarUsuario();
   }
-  cargarUsuario(){
-    this.cargando=true;
+  cargarUsuario() {
+    this.cargando = true;
 
-    this.usuarioService.cargarUsuario(this.desde).subscribe(({total, usuarios})=>{
-      this.totalUsuarios=total;
-      this.usuarios=usuarios;
-      this.cargando=false;
+    this.usuarioService.cargarUsuario(this.desde).subscribe(({ total, usuarios }) => {
+      this.totalUsuarios = total;
+      this.usuarios = usuarios;
+      this.usuariosTemp = usuarios;
+      this.cargando = false;
     })
   }
 
-  cambiarPagina(valor:number){
-    this.desde+=valor;
-    if(this.desde<0){
-      this.desde=0;
-    }else if(this.desde>this.totalUsuarios){
-      this.desde-=valor;
+  cambiarPagina(valor: number) {
+    this.desde += valor;
+    if (this.desde < 0) {
+      this.desde = 0;
+    } else if (this.desde > this.totalUsuarios) {
+      this.desde -= valor;
     }
     this.cargarUsuario();
+  }
+  buscar(termino: string) {
+    if (termino.length == 0) {
+      this.usuarios = this.usuariosTemp;
+    } else {
+      this.busquedaService.buscar('usuarios', termino).subscribe(resp => {
+        this.usuarios = resp;
+      })
+    }
   }
 
 }
