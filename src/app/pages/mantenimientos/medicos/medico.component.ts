773d00b18f780bd76e5d02aa197a898014b6b877
unknown <luis.arispe1991@gmail.com>
2021-06-15 10:01:47 -0400

    finalizando editar medico

--- src/app/pages/mantenimientos/medicos/medico.component.ts
+++ src/app/pages/mantenimientos/medicos/medico.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Hospital } from 'src/app/models/hospital.model';
+import { Medico } from 'src/app/models/medico.model';
+import Swal from 'sweetalert2';
 import { HospitalService } from '../../../services/hospital.service';
+import { MedicoService } from '../../../services/medico.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CargarUsuario } from '../../../interfaces/cargar-usuarios.interface';
 
 @Component({
   selector: 'app-medico',
@@ -13,16 +18,32 @@ export class MedicoComponent implements OnInit {
 
   public medicoForm!: FormGroup;
   public hospitales: Hospital[] = [];
-  constructor(private fb: FormBuilder, private hospitalService: HospitalService) { }
+  public hospitalSelecciona?:Hospital;
+  public medicoSeleccionado?:Medico;
+  constructor(private fb: FormBuilder, private hospitalService: HospitalService, private medicoService: MedicoService, private router:Router,private activateRouter: ActivatedRoute) { }
 
   ngOnInit(): void {
+
+    this.activateRouter.params.subscribe(({id})=>{
+      this.cargarMedico(id)
+    })
+
     this.medicoForm = this.fb.group({
-      nombre: ['Hernando', Validators.required],
+      nombre: ['', Validators.required],
       hospital: ['', Validators.required]
     })
     this.cargarHospitales()
-  }
 
+    this.medicoForm.get('hospital')?.valueChanges.subscribe(hospitalId=>{
+      
+        this.hospitalSelecciona=this.hospitales.find(h=>h._id===hospitalId)
+    })
+  }
+  cargarMedico(id:string){
+    this.medicoService.obtenerMedicoPorId(id).subscribe(medico=>{
+      this.medicoSeleccionado=medico;
+    })
+  }
   cargarHospitales() {
     this.hospitalService.cargarHospitales().subscribe((hospitales: Hospital[]) => {
       this.hospitales = hospitales;
@@ -30,8 +51,11 @@ export class MedicoComponent implements OnInit {
   }
 
   guardarMedico() {
-
-    console.log(this.medicoForm.value);
+    this.medicoService.crearMedico(this.medicoForm.value).subscribe((resp:any)=>{
+      Swal.fire('','médico creado correctamente.', 'success');
+      this.router.navigateByUrl(`/dashboard/medico/${resp.medico._id}`)
+    });
+    
   }
 
 
--- src/app/services/medico.service.ts
+++ src/app/services/medico.service.ts
@@ -32,7 +32,15 @@ export class MedicoService {
     );
 
   }
-  crearMedico(medico:Medico) {
+  obtenerMedicoPorId(id:string) {
+
+    const url = `${base_url}/medicos/${id}`;
+    return this.http.get<{ ok: boolean, medico: Medico }>(url, this.headers).pipe(
+      map((resp: { ok: boolean, medico: Medico }) => resp.medico)
+    );
+
+  }
+  crearMedico(medico:{nombre:string, hospital:string}) {
 
     const url = `${base_url}/medicos`;
     return this.http.post(url,medico, this.headers);
